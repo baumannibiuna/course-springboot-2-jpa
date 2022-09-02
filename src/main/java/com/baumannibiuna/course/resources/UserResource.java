72d711b3ebb9194dbf1e17988d88fd231995437a
baumannibiuna <baumannibiuna@gmail.com>
2022-09-01 21:14:43 -0300

    User entity and resource

---
+++ src/main/java/com/baumannibiuna/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.baumannibiuna.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baumannibiuna.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "999955555", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
