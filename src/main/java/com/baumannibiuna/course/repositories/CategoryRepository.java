86b41a54dfd3dce307d8a954d6b83ffdc5a9960b
baumannibiuna <baumannibiuna@gmail.com>
2022-09-05 18:38:43 -0300

    Category

---
+++ src/main/java/com/baumannibiuna/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+ package com.baumannibiuna.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.baumannibiuna.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
