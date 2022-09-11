e7874ecb32c5646464a3c2d18f0c5e19a1082c3e
baumannibiuna <baumannibiuna@gmail.com>
2022-09-10 22:01:48 -0300

    Payment, one-to-one association

--- src/main/java/com/baumannibiuna/course/config/TestConfig.java
+++ src/main/java/com/baumannibiuna/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.baumannibiuna.course.entities.Category;
 import com.baumannibiuna.course.entities.Order;
 import com.baumannibiuna.course.entities.OrderItem;
+import com.baumannibiuna.course.entities.Payment;
 import com.baumannibiuna.course.entities.Product;
 import com.baumannibiuna.course.entities.User;
 import com.baumannibiuna.course.entities.enums.OrderStatus;
@@ -82,6 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
